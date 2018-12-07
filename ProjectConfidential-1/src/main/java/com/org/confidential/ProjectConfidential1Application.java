50e769c835a0bca63ee12c08939a8da9ad78b918
VIJAY_KUMAR\TRICIEL-04 <vijay.kumar@triciel.com>
2018-12-07 14:24:23 +0530

    test commit

---
+++ ProjectConfidential-1/src/main/java/com/org/confidential/ProjectConfidential1Application.java
@@ -0,0 +1,12 @@
+package com.org.confidential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectConfidential1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectConfidential1Application.class, args);
+	}
+}
